a8b8d474dfffaee85ef15395f210c3eb84d143e7
krishnanshu01 <krishnanshum41@gmail.com>
2020-11-09 14:25:25 +0530

    Done

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require("fs")
-const port = 3000;
+let port = process.env.PORT || 8080;//ye rha port
+//
 const bodyparser = require("body-parser")
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/details', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://krishnanshu:krish143@cluster0.wxqsk.mongodb.net/krishnanshu?retryWrites=true&w=majority', {useNewUrlParser: true});
 //Define mangoose schema
 var details = new mongoose.Schema({
     name: String,
@@ -16,6 +17,7 @@ var details = new mongoose.Schema({
   });
   const detail = mongoose.model('detail', details);
 //add to use pug
+
 app.use(express.urlencoded());
 app.use('/static' , express.static('static'));
 const pug = require('pug');
@@ -26,10 +28,13 @@ app.set('view engine' , 'pug');
 app.set('views' ,path.join(__dirname,'views'));
 
 app.get('/' , (req , res) =>{
-    const con = 'this is the best place where you can directly contact me without any problem';
-    const params = {'title' : 'pubG is a best game not', 'content': con}
+    const params = {'title' : 'pubG is a best game not'}
     res.status(200).render('index.pug', params)
 })
+app.get('/defaultrout' , (req , res) =>{
+    res.status(200).json({'hello': 'hekki wooerhossd'})
+})
+// yha schema bna kr main mongo db mai dala hi
 app.post('/' , (req , res) =>{
     var mydata = new detail(req.body);
     mydata.save().then(() =>{
@@ -38,7 +43,10 @@ app.post('/' , (req , res) =>{
         res.status(400).send("not saved")
     });
 })
-
+//listern port ye rha
 app.listen(port, () => {
-    console.log(`The application started successfully on port ${port}`);
+    console.log(`The application started successfully on port `);
 })
+//kya missing lg rha isme trko
+//kyoke bhai ye first time hi mai koi site bna rha hu toh boht saari mistakes kr rha hougnga
+//pr local host pe sahi kam kr rhi hi mre site
